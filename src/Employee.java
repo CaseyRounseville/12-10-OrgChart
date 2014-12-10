53fb1a549408d89799c8484c58084a175c7a15d9
CaseyRounseville <mariarounseville@cox.net>
2014-12-10 08:17:58 -0700

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,1 +1,8 @@
 // class to represent an emplayee - inherits Person
+public class Employee extends Person
+{
+  private double hourlyWage;
+  private int hoursWorked;
+  private String jobTitle;
+  private Manager manager=new Manager();
+  public Employee(String s1,String s2,int a,double wage,
