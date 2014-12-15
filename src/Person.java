86a50f8ce05e5f8e22639b14f756f3d7fd04095c
CaseyRounseville <mariarounseville@cox.net>
2014-12-15 08:18:27 -0700

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -16,6 +16,15 @@ public class Person
     lastName=s2;
     age=a;
   }
+  public boolean equals(Object obj)/////////////////////////////////////////////////////////////////////////////////////////
+  {
+    if(obj instanceof Person)
+    {
+      Person other=(Person)obj;
+      return this.getFirstName()==other.getFirstName()&&this.getLastName()==other.getLastName()&&this.getAge()==other.getAge();
+    }
+    return false;
+  }
   public String getFirstName()
   {
     return firstName;
